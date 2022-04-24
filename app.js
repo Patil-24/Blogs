e01cccd10d1a18da01a75463e09ea20aeb8fcee3
Iranna Patil <patiliranna24@gmail.com>
2022-04-24 18:18:33 +0530

    Second commit

---
+++ app.js
@@ -0,0 +1,80 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const homeStartingContent = "Welocome to my Blog Website here you will get all kind of Programming Language"
+const aboutContent = "Welcome to Tech Blogs, your number one source for all things related to tech Programming. We're dedicated to giving you the very best of Content, with a focus on easy to understand,upadated Content,with practical oriented Examples.Founded in 2022 by Iranna Patil, has come a long way from its beginnings in a College.We hope you enjoy our Blog as much as we enjoy offering them to you. If you have any questions or comments, please don't hesitate to contact us.Sincerely,Iranna Patil,[Founder, Tech Blog]"
+const contactContent = "Contact me via various Social Platforms";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb+srv://Iranna24:irannapatil@cluster0.iefat.mongodb.net/blog", {useNewUrlParser: true,useUnifiedTopology: true });
+
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+app.get("/", function(req, res){
+
+  Post.find({}, function(err, posts){
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+      });
+  });
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+
+
+  post.save(function(err){
+    if (!err){
+        res.redirect("/");
+    }
+  });
+});
+
+app.get("/posts/:postId", function(req, res){
+
+const requestedPostId = req.params.postId;
+
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  });
+
+});
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
